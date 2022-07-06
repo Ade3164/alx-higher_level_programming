8383451225f4fe326785ed46a4a36c9eb08a300a
Ade3164 <adebayoidris051@gmail.com>
2022-07-06 00:57:13 +0000

    python

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-def number_of_lines(filename=""):
-    """counts number of lines"""
-    with open(filename, mode="r", encoding="utf-8") as f:
-        lineNum = 0
-        for line in f:
-            lineNum += 1
-    return (lineNum)
+"""Defines a file-writing function."""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)

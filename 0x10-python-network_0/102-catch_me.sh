98b2324c1d61f7e7207bf1b7360029e987c3af7b
Ade3164 <adebayoidris051@gmail.com>
2022-09-23 16:35:09 +0100

    update

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that causes the server to respond
-curl -s 0.0.0.0:5000/catch_me -X PUT -L -d "user_id=98" -H "Origin: You got me!"
+curl -sLX PUT -H "origin:You got me!" -d "user_id=98" 0.0.0.0:5000/catch_me

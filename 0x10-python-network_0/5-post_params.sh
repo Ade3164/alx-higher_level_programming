ad65fdac099b78823af4fb36af97d60cdd9442ba
root <root@ADEBAYO.localdomain>
2022-09-23 14:33:18 +0100

    update

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends post request with 2 variables
-curl -sd 'email=test@gmail.com=I will always be here for PLD' "$1"
+# Script that takes in a url, sends a POST request to the passed URL and display the body of the response
+curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" $1
